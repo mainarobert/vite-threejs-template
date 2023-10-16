4bd632bae6c7e5e371d5f46680368c37aec264d1
Robert Maina <robertmaina254@gmail.com>
2023-10-16 18:12:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ const scene = new THREE.Scene()
 // Geometry
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 // Camera 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
 camera.position.z = 3
-camera.lookAt(cube)
+camera.lookAt(mesh)
 scene.add(camera)
 
 // Controls
